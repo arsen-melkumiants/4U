69f8d17ccd7bb32989f5fdd57d53a057057b2c57
Arsen Melkumiants <arsen.mem@gmail.com>
2014-04-03 02:36:43 +0300

    update permision

--- application/controllers/admin/manage_currency.php
+++ application/controllers/admin/manage_currency.php
@@ -30,7 +30,7 @@ class manage_currency extends CI_Controller {
 	function __construct() {
 		parent::__construct();
 		$this->load->library('ion_auth');
-		if (!$this->ion_auth->logged_in()) {
+		if (!$this->ion_auth->is_admin()) {
 			redirect(ADM_URL.'auth/login');
 		}
 
--- application/controllers/admin/manage_product.php
+++ application/controllers/admin/manage_product.php
@@ -30,7 +30,7 @@ class Manage_product extends CI_Controller {
 	function __construct() {
 		parent::__construct();
 		$this->load->library('ion_auth');
-		if (!$this->ion_auth->logged_in()) {
+		if (!$this->ion_auth->is_admin()) {
 			redirect(ADM_URL.'auth/login');
 		}
 
