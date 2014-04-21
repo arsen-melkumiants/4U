21e7d6d428b95ec7278d5679793fb7d7ab35680c
Arsen Melkumiants <arsen.mem@gmail.com>
2014-04-22 01:55:55 +0300

    add hover frame

--- application/views/product_block.php
+++ application/views/product_block.php
@@ -6,9 +6,13 @@
 	<?php foreach ($products as $item) {?>
 	<div class="item">
 		<h4 class="name"><a href="<?php echo product_url($item['id'], $item['name'])?>"><?php echo $item['name']?></a></h4>
-		<div class="image">
-			<?php echo !empty($item['file_name']) ? '<img src="/uploads/gallery/small_thumb/'.$item['file_name'].'" />' : '';?>
-		</div>
+		<a href="<?php echo product_url($item['id'], $item['name'])?>">
+			<div class="image">
+				<?php echo !empty($item['file_name']) ? '<img src="/uploads/gallery/small_thumb/'.$item['file_name'].'" />' : '';?>
+				<div class="bg"></div>
+				<div class="bg_text">Read more</div>
+			</div>
+		</a>
 		<div class="action">
 		<div class="price"><i class="c_icon_label"></i><?php echo $item['price'].' '.$item['symbol']?></div>
 		<button class="orange_btn add_to_cart" 
