869a0e3d0543dba4faf1a0e758089e6670f3165d
Arsen Melkumiants <arsen.mem@gmail.com>
2014-04-23 01:25:56 +0300

    add new view mode

--- application/views/category.php
+++ application/views/category.php
@@ -59,6 +59,16 @@
 					</div>
 				</div>
 			<?php } else {?>
+				<div class="item list">
+					<h4 class="name"><a href="<?php echo product_url($item['id'], $item['name'])?>"><?php echo $item['name']?></a></h4>
+					<div class="price"><i class="c_icon_label"></i><?php echo $item['price'].' '.$item['symbol']?></div>
+					<button class="orange_btn add_to_cart"
+						data-name="<?php echo $item['name']?>"
+						data-id="<?php echo $item['id']?>"
+						data-href="<?php echo product_url($item['id'], $item['name'])?>"
+					>Buy Now</button>
+					<div class="clear"></div>
+				</div>
 			<?php } ?>
 	<?php }?>
 	<div class="clear"></div>
