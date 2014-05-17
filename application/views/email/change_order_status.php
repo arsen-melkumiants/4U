b0d07e13d3bc279bb4aa9e10968ca1ec812c62d1
Arsen Melkumiants <arsen.mem@gmail.com>
2014-05-17 18:12:50 +0300

    translate mails

--- application/views/email/change_order_status.php
+++ application/views/email/change_order_status.php
@@ -1,9 +1,23 @@
 <html>
-<body>
-	<h4>Заказ №<?php echo $id?></h4>
-    <p>Статус Вашего заказ был обновлён на <b>"<?php echo $s_name?>"</b>.</p>
-    <p>Изменения по заказу Вы можете просмотреть в личном кабинете.</p>
-    <br />
-    <p>С уважением, Администрация сайта <a href="<?php echo base_url()?>"><?php echo SITE_NAME?></a></p>
-</body>
+	<body>
+		<?php if (empty($_COOKIE['user_lang']) || $_COOKIE['user_lang'] == 'ru') {?>
+		<h4>Заказ №<?php echo $id?></h4>
+		<p>Статус Вашего заказ был обновлён.</p>
+		<p>Изменения по заказу Вы можете просмотреть в личном кабинете.</p>
+		<br />
+		<p>С уважением, Администрация сайта <a href="<?php echo base_url()?>"><?php echo SITE_NAME?></a></p>
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<?php }?>
+		<?php if (empty($_COOKIE['user_lang']) || $_COOKIE['user_lang'] == 'en') { ?>
+		<h4>Order №<?php echo $id?></h4>
+		<p>Your order status has been updated.</p>
+		<p>changes of order you can see in your personal account.</p>
+		<br />
+		<p>Sincerely, administration of this site <a href="<?php echo base_url()?>"><?php echo SITE_NAME?></a></p>
+		<?php }?>
+	</body>
 </html>
--- application/views/email/create_order.php
+++ application/views/email/create_order.php
@@ -1,14 +1,35 @@
 <html>
-<body>
-	<h4>Заказ успешно принят</h4>
-	<p>Ваш заказ(номер №<?php echo $order_id?>) принят и будет рассмотрен в ближайшем времени.</p>
-    <p>Статус заказа Вы можете просмотреть в личном кабинете</p>
-    <?php if($auto_reg){?>
-    <br />
-    <p>Вы не зарегистрированы в системе. Не стоит беспокоиться, <font color="red">регистрация продёт автоматически</font>, а Вашим паролем будет "<font color="red"><?php echo $email?></font>"</p>
-    <p>После первого входа в систему <font color="red">в целях безопасности рекомендуем Вам сменить пароль</font></p>
-    <?php }?>
-    <br />
-    <p>С уважением, Администрация сайта <a href="<?php echo base_url()?>"><?php echo SITE_NAME?></a></p>
-</body>
+	<body>
+		<?php if (empty($_COOKIE['user_lang']) || $_COOKIE['user_lang'] == 'ru') {?>
+		<h4>Заказ успешно принят</h4>
+		<p>Ваш заказ(номер №<?php echo $order_id?>) принят и будет рассмотрен в ближайшем времени.</p>
+		<p>Статус заказа Вы можете просмотреть в личном кабинете</p>
+		<?php if($auto_reg){?>
+		<br />
+		<p>Вы не зарегистрированы в системе. Не стоит беспокоиться, <font color="red">регистрация продёт автоматически</font>, а Вашим паролем будет "<font color="red"><?php echo $email?></font>"</p>
+		<p>После первого входа в систему <font color="red">в целях безопасности рекомендуем Вам сменить пароль</font></p>
+		<?php }?>
+		<br />
+		<p>С уважением, Администрация сайта <a href="<?php echo base_url()?>"><?php echo SITE_NAME?></a></p>
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<?php }?>
+
+
+		<?php if (empty($_COOKIE['user_lang']) || $_COOKIE['user_lang'] == 'en') { ?>
+		<h4>Orders received successfully</h4>
+		<p>Ваш заказ №<?php echo $order_id?> принят и будет рассмотрен в ближайшем времени.Your order №<?php echo $order_id?> accepted and will be considered in the near future.</p>
+		<p>You can see order status in your personal account</p>
+		<?php if($auto_reg){?>
+		<br />
+		<p>You are not registered in the system. Do not worry, <font color="red"> registration threaded automatically </font>, and your password will be "<font color="red"> <?php echo $ email?></font>"</p>
+		<p>After the first login <font color="red">for security reasons we recommend you change your password</font></p>
+		<?php }?>
+		<br />
+		<p>Sincerely, administration of this site <a href="<?php echo base_url()?>"><?php echo SITE_NAME?></a></p>
+		<?php }?>
+	</body>
 </html>
