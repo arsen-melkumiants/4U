c50efbb4c47a08d2ebe3aa7be9346799777ac0ff
Iriska <iryna.turkina@gmail.com>
2014-05-17 23:36:34 +0300

    Fixed send mail 2

--- application/views/email/account_reffiled.php
+++ application/views/email/account_reffiled.php
@@ -2,7 +2,7 @@
 	<body>
 		<?php if (empty($_COOKIE['user_lang']) || $_COOKIE['user_lang'] == 'ru') {?>
 		<h4><?php echo lang('mail_account_reffiled');?></h4>
-		<p>Поздравляем! Ваш Счет пополнен на сумму <?php echo $amount.$currency;?>.</p>
+		<p>Поздравляем! Ваш Счет пополнен на сумму <?php echo $amount;?>$.</p>
 		<p>Детали Вы можете просмотреть в личном кабинете.</p>
 		<br />
 		<p>С уважением, Администрация сайта <a href="<?php echo base_url()?>"><?php echo SITE_NAME?></a></p>
@@ -16,7 +16,7 @@
 
 		<?php if (empty($_COOKIE['user_lang']) || $_COOKIE['user_lang'] == 'en') { ?>
 		<h4><?php echo lang('mail_account_reffiled');?></h4>
-		<p>Congratulations! Your Account has been refilled for the amount <?php echo $amount.$currency;?>.</p>
+		<p>Congratulations! Your Account has been refilled for the amount <?php echo $amount;?>$.</p>
 		<p>You can see details in your personal cabinet</p>
 		<br />
 		<p>Sincerely, administration of this site <a href="<?php echo base_url()?>"><?php echo SITE_NAME?></a></p>
--- application/views/email/product_purchased.php
+++ application/views/email/product_purchased.php
@@ -2,7 +2,7 @@
 	<body>
 		<?php if (empty($_COOKIE['user_lang']) || $_COOKIE['user_lang'] == 'ru') {?>
 		<h4><?php echo lang('mail_product_purchased');?></h4>
-		<p>Поздравляем! Ваш товар (номер №<?php echo $type_id?>) куплен на сумму с учетом комиссии <?php echo $amount.$currency;?>.</p>
+		<p>Поздравляем! Ваш товар (номер №<?php echo $type_id?>) куплен на сумму с учетом комиссии <?php echo $amount;?>$.</p>
 		<p>Детали Вы можете просмотреть в личном кабинете.</p>
 		<br />
 		<p>С уважением, Администрация сайта <a href="<?php echo base_url()?>"><?php echo SITE_NAME?></a></p>
@@ -16,7 +16,7 @@
 
 		<?php if (empty($_COOKIE['user_lang']) || $_COOKIE['user_lang'] == 'en') { ?>
 		<h4><?php echo lang('mail_product_purchased');?></h4>
-		<p>Congratulations! Your product (№<?php echo $type_id?>) purchased for the sum view of the commission <?php echo $amount.$currency;?>.</p>
+		<p>Congratulations! Your product (№<?php echo $type_id?>) purchased for the sum view of the commission <?php echo $amount;?>$.</p>
 		<p>You can see details in your personal cabinet</p>
 		<br />
 		<p>Sincerely, administration of this site <a href="<?php echo base_url()?>"><?php echo SITE_NAME?></a></p>
