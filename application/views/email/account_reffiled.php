8f47b315dcd1e79fa86515894efc523d963867e3
Iriska <iryna.turkina@gmail.com>
2014-05-17 23:34:42 +0300

    Fixed send mail

--- application/views/email/account_reffiled.php
+++ application/views/email/account_reffiled.php
@@ -4,11 +4,6 @@
 		<h4><?php echo lang('mail_account_reffiled');?></h4>
 		<p>Поздравляем! Ваш Счет пополнен на сумму <?php echo $amount.$currency;?>.</p>
 		<p>Детали Вы можете просмотреть в личном кабинете.</p>
-		<?php if($auto_reg){?>
-		<br />
-		<p>Вы не зарегистрированы в системе. Не стоит беспокоиться, <font color="red">регистрация продёт автоматически</font>, а Вашим паролем будет "<font color="red"><?php echo $email?></font>"</p>
-		<p>После первого входа в систему <font color="red">в целях безопасности рекомендуем Вам сменить пароль</font></p>
-		<?php }?>
 		<br />
 		<p>С уважением, Администрация сайта <a href="<?php echo base_url()?>"><?php echo SITE_NAME?></a></p>
 		<br />
@@ -23,11 +18,6 @@
 		<h4><?php echo lang('mail_account_reffiled');?></h4>
 		<p>Congratulations! Your Account has been refilled for the amount <?php echo $amount.$currency;?>.</p>
 		<p>You can see details in your personal cabinet</p>
-		<?php if($auto_reg){?>
-		<br />
-		<p>You are not registered in the system. Do not worry, <font color="red"> registration threaded automatically </font>, and your password will be "<font color="red"> <?php echo $email?></font>"</p>
-		<p>After the first login <font color="red">for security reasons we recommend you change your password</font></p>
-		<?php }?>
 		<br />
 		<p>Sincerely, administration of this site <a href="<?php echo base_url()?>"><?php echo SITE_NAME?></a></p>
 		<?php }?>
--- application/views/email/product_no_moderated.php
+++ application/views/email/product_no_moderated.php
@@ -4,11 +4,6 @@
 		<h4><?php echo lang('mail_product_no_moderation');?></h4>
 		<p>К сожалению Ваш товар (номер №<?php echo $id?>) не прошел модерацию.</p>
 		<p>Узнать причину ошибки или дополнительную информацию Вы можете обратившись к администратору: <?php echo $email;?>.</p>
-		<?php if($auto_reg){?>
-		<br />
-		<p>Вы не зарегистрированы в системе. Не стоит беспокоиться, <font color="red">регистрация продёт автоматически</font>, а Вашим паролем будет "<font color="red"><?php echo $email?></font>"</p>
-		<p>После первого входа в систему <font color="red">в целях безопасности рекомендуем Вам сменить пароль</font></p>
-		<?php }?>
 		<br />
 		<p>С уважением, Администрация сайта <a href="<?php echo base_url()?>"><?php echo SITE_NAME?></a></p>
 		<br />
@@ -23,11 +18,6 @@
 		<h4><?php echo lang('mail_product_no_moderation');?></h4>
 		<p>Unfortunately your product (№<?php echo $order_id?>) not passed moderation.</p>
 		<p>Contact the administrator for more information: <?php echo $email;?>.</p>
-		<?php if($auto_reg){?>
-		<br />
-		<p>You are not registered in the system. Do not worry, <font color="red"> registration threaded automatically </font>, and your password will be "<font color="red"> <?php echo $email?></font>"</p>
-		<p>After the first login <font color="red">for security reasons we recommend you change your password</font></p>
-		<?php }?>
 		<br />
 		<p>Sincerely, administration of this site <a href="<?php echo base_url()?>"><?php echo SITE_NAME?></a></p>
 		<?php }?>
