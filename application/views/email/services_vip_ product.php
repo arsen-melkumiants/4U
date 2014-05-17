532cb8bebd1b2e0ec769177d6b4182ec7408b690
Iriska <iryna.turkina@gmail.com>
2014-05-17 22:17:38 +0300

    add mail services

---
+++ application/views/email/services_vip_ product.php
@@ -0,0 +1,35 @@
+<html>
+	<body>
+		<?php if (empty($_COOKIE['user_lang']) || $_COOKIE['user_lang'] == 'ru') {?>
+		<h4>Оплата услуги "Vip товар"</h4>
+		<p>Оплата услуги "Vip товар" выполнена успешно.</p>
+		<p>Детали Вы можете просмотреть в личном кабинете.</p>
+		<?php if($auto_reg){?>
+		<br />
+		<p>Вы не зарегистрированы в системе. Не стоит беспокоиться, <font color="red">регистрация продёт автоматически</font>, а Вашим паролем будет "<font color="red"><?php echo $email?></font>"</p>
+		<p>После первого входа в систему <font color="red">в целях безопасности рекомендуем Вам сменить пароль</font></p>
+		<?php }?>
+		<br />
+		<p>С уважением, Администрация сайта <a href="<?php echo base_url()?>"><?php echo SITE_NAME?></a></p>
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<?php }?>
+
+
+		<?php if (empty($_COOKIE['user_lang']) || $_COOKIE['user_lang'] == 'en') { ?>
+		<h4>Payment services "Vip product"</h4>
+		<p>Payment services "Vip product" completed successfully.</p>
+		<p>You can see details in your personal cabinet</p>
+		<?php if($auto_reg){?>
+		<br />
+		<p>You are not registered in the system. Do not worry, <font color="red"> registration threaded automatically </font>, and your password will be "<font color="red"> <?php echo $email?></font>"</p>
+		<p>After the first login <font color="red">for security reasons we recommend you change your password</font></p>
+		<?php }?>
+		<br />
+		<p>Sincerely, administration of this site <a href="<?php echo base_url()?>"><?php echo SITE_NAME?></a></p>
+		<?php }?>
+	</body>
+</html>
