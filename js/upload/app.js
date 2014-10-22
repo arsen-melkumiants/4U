dce18c3ad6bb0e19d1d54f703a2bfacd31c5521d
Arsen Melkumiants <arsen.mem@gmail.com>
2014-10-22 23:25:59 +0300

    disable previews

--- js/upload/app.js
+++ js/upload/app.js
@@ -13,77 +13,78 @@
 /* global window, angular */
 
 (function () {
-    'use strict';
+	'use strict';
 
-    var url = upload_url;
+	var url = upload_url;
 
-    angular.module('demo', [
-        'blueimp.fileupload'
-    ])
-        .config([
-            '$httpProvider', 'fileUploadProvider',
-            function ($httpProvider, fileUploadProvider) {
-                delete $httpProvider.defaults.headers.common['X-Requested-With'];
-                fileUploadProvider.defaults.redirect = window.location.href.replace(
-                    /\/[^\/]*$/,
-                    '/cors/result.html?%s'
-                );
-            }
-        ])
+	angular.module('demo', [
+		'blueimp.fileupload'
+	])
+	.config([
+		'$httpProvider', 'fileUploadProvider',
+		function ($httpProvider, fileUploadProvider) {
+			delete $httpProvider.defaults.headers.common['X-Requested-With'];
+			fileUploadProvider.defaults.redirect = window.location.href.replace(
+				/\/[^\/]*$/,
+				'/cors/result.html?%s'
+			);
+		}
+	])
 
-        .controller('DemoFileUploadController', [
-            '$scope', '$http', '$filter', '$window',
-            function ($scope, $http) {
-                $scope.options = {
-                    url: url
-                };
-                    $scope.loadingFiles = true;
-                    $http.get(url)
-                        .then(
-                            function (response) {
-                                $scope.loadingFiles = false;
-                                $scope.queue = response.data.files || [];
-                            },
-                            function () {
-                                $scope.loadingFiles = false;
-                            }
-                        );
-            }
-        ])
+	.controller('DemoFileUploadController', [
+		'$scope', '$http', '$filter', '$window',
+		function ($scope, $http) {
+			$scope.options = {
+				url: url,
+				disableImagePreview : true
+			};
+			$scope.loadingFiles = true;
+			$http.get(url)
+			.then(
+				function (response) {
+					$scope.loadingFiles = false;
+					$scope.queue = response.data.files || [];
+				},
+				function () {
+					$scope.loadingFiles = false;
+				}
+			);
+		}
+	])
 
-        .controller('FileDestroyController', [
-            '$scope', '$http',
-            function ($scope, $http) {
-                var file = $scope.file,
-                    state;
-                if (file.url) {
-                    file.$state = function () {
-                        return state;
-                    };
-                    file.$destroy = function () {
-                        state = 'pending';
-						return $http({
-							url: file.deleteUrl,
-						   method: file.deleteType,
-						   headers:  {
-							   'X-Requested-With' : 'XMLHttpRequest'
-						   }
-						}).then(
-                            function () {
-                                state = 'resolved';
-                                $scope.clear(file);
-                            },
-                            function () {
-                                state = 'rejected';
-                            }
-                        );
-                    };
-                } else if (!file.$cancel && !file._index) {
-                    file.$cancel = function () {
-                        $scope.clear(file);
-                    };
-                }
-            }
-        ]);
+	.controller('FileDestroyController', [
+		'$scope', '$http',
+		function ($scope, $http) {
+			var file = $scope.file,
+			state;
+			if (file.url) {
+				file.$state = function () {
+					return state;
+				};
+				file.$destroy = function () {
+					state = 'pending';
+					return $http({
+						url: file.deleteUrl,
+						method: file.deleteType,
+						headers:  {
+							'X-Requested-With' : 'XMLHttpRequest'
+						}
+					}).then(
+						function () {
+							state = 'resolved';
+							$scope.clear(file);
+						},
+						function () {
+							state = 'rejected';
+						}
+					);
+				};
+			} else if (!file.$cancel && !file._index) {
+				file.$cancel = function () {
+					$scope.clear(file);
+				};
+			}
+		}
+	]);
 
 }());
